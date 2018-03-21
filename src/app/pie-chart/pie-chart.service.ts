2082a1883c9e6db41d3ace068efc2a4592c53d72
Marc Bogaerts <marc.bogaerts@ing.be>
2018-03-21 17:44:27 +0100

    Create data

--- src/app/pie-chart/pie-chart.service.ts
+++ src/app/pie-chart/pie-chart.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { PieChartSliceInfo } from './pie-chart-slice-info';
+import * as randomColor from 'randomColor';
 
 @Injectable()
 export class PieChartService {
@@ -7,6 +8,39 @@ export class PieChartService {
   constructor() { }
 
   getPieChartViewData(pieChartSlices: PieChartSliceInfo[]): any {
+    const labels = [];
+    const data = [];
+    const bgColors = [];
+    const hoverBgColors = [];
+    for (const slice of pieChartSlices) {
+      labels.concat(slice.name);
+      data.concat(slice.value);
+      bgColors.concat(randomColor());
+      hoverBgColors.concat(randomColor());
+    }
 
+    return {
+      labels: labels,
+      datasets: [
+        {
+          data: data,
+          backgroundColor: bgColors,
+          hoverBackgroundColor: hoverBgColors
+        }
+      ]
+    };
+  }
+
+  private pieChartViewData(slice: PieChartSliceInfo) : any {
+    return null;
+  }
+
+  getPieChartSliceInfo(): PieChartSliceInfo[] {
+    return [
+      new PieChartSliceInfo('Savings', 6000),
+      new PieChartSliceInfo('Food', 750),
+      new PieChartSliceInfo('Rent', 800),
+      new PieChartSliceInfo('Car', 400),
+    ];
   }
 }
